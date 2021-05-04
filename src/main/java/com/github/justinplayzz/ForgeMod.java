e4df7dc1c8cb0e07db2ad07d0d71b3d35066426b
JustinPlayzz <75293234+JustinPlayzz@users.noreply.github.com>
2021-05-04 14:41:14 -0400

    Items starter

--- src/main/java/com/github/justinplayzz/ForgeMod.java
+++ src/main/java/com/github/justinplayzz/ForgeMod.java
@@ -1,9 +1,11 @@
 package com.github.justinplayzz;
 
+import com.github.justinplayzz.core.init.ItemInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -19,15 +21,18 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("tutorialforge")
+@Mod(ForgeMod.MOD_ID)
 public class ForgeMod {
     // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "tutorialforge";
 
     public ForgeMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register ourselves for server and other game events we are interested in
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+
+        ItemInit.ITEMS.register(bus);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
--- src/main/java/com/github/justinplayzz/core/init/ItemInit.java
+++ src/main/java/com/github/justinplayzz/core/init/ItemInit.java
@@ -1,4 +1,17 @@
 package com.github.justinplayzz.core.init;
 
+import com.github.justinplayzz.ForgeMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ItemInit {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            ForgeMod.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY_ITEM = ITEMS.register("ruby_item",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
 }
